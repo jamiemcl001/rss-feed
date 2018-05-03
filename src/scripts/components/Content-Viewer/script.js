8a857ac794c76eb27b1890da84e8fa8b79339a37
jamiemcl001 <jamiemcl001@gmail.com>
2018-05-03 06:54:43 +0100

    remove calculation of md5 hash and watcher as it is no longer needed

--- src/scripts/components/Content-Viewer/script.js
+++ src/scripts/components/Content-Viewer/script.js
@@ -1,4 +1,5 @@
 import orderby from 'orderby';
+import Vue from 'vue';
 import ArticleCard from '@components/Article-Card/index.vue';
 
 export default {
@@ -10,14 +11,13 @@ export default {
             return orderby(this.visibleArticles, 'pubDateMilliseconds');
         },
         visibleArticles() {
+            Vue.nextTick().then(() => {
+                this.$redrawVueMasonry();
+            });
+
             return this.$store.state.feeds
                 .filter((feed) => feed.isActive)
                 .reduce((acc, val) => [...acc, ...val.feedContent], []);
         }
-    },
-    data() {
-        return {
-            numberOfColumns: 0
-        };
     }
 };
