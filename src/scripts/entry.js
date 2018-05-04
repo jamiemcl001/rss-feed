4dcf5de2328ded98de01f9a07def5cf029217073
jamiemcl001 <jamiemcl001@gmail.com>
2018-05-04 05:05:13 +0100

    add offline-js for network checking

--- src/scripts/entry.js
+++ src/scripts/entry.js
@@ -1,3 +1,4 @@
+import 'offline-js';
 import Vue from 'vue';
 import {VueMasonryPlugin} from 'vue-masonry';
 import VueTruncate from 'vue-truncate-filter';
@@ -7,10 +8,15 @@ import store from './store';
 Vue.use(VueMasonryPlugin);
 Vue.use(VueTruncate);
 
-store.dispatch('feeds/fetch-latest-items');
+Offline.options = {
+  checkOnLoad: true,
+  requests: true
+};
 
 new Vue({
     el: '#app',
     render: (h) => h(App),
     store
 });
+
+store.dispatch('feeds/fetch-latest-items');
