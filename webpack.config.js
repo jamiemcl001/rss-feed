04d272126f21e56c5aa7bc7e349ea3b685cd446f
jamiemcl001 <jamiemcl001@gmail.com>
2018-05-05 03:33:51 +0100

    add postcss loader for compatibility purposes

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ module.exports = [
                     use: [
                         MiniCssExtractPlugin.loader,
                         'css-loader',
+                        'postcss-loader',
                         'sass-loader',
                     ]
                 },
